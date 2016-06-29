9e9984f8f1c858d4cef13de3ec9578693588f44b
James Hayhurst <James.A.Hayhurst@gmail.com>
2016-06-29 03:18:20 -0400

    wrote initial set value function - not sure if works yet

--- error.go
+++ error.go
@@ -3,42 +3,46 @@ package bossy
 import "fmt"
 
 // Err - the type used within my application for error handling
-type Error struct {
-	Code     int
-	path     string
-	valType  string
-	value    interface{}
-	innerErr error
+type Error interface {
+	Error() string
+}
+
+type NewError struct {
+	Code       int
+	path       string
+	valType    string
+	value      interface{}
+	innerError error
 }
 
 // Upgrades an external error making it a error
 func UpgradeError(e error) Error {
-	return Error{Code: ErrUpgradedError, deepErr: e}
+	return NewError{Code: ErrUpgradedError, innerError: e}
 }
 
 // the function `Error` to make my custom errors work
-func (e NewErr) Error() string {
+func (e NewError) Error() string {
 	switch {
 	case e.Code == ErrUpgradedError:
 		return e.innerError.Error()
-	case e.path != "" && e.valType != "" && e.value != nil && e.innerErr != nil:
-		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType, e.value, e.innerErr)
+	case e.path != "" && e.valType != "" && e.value != nil && e.innerError != nil:
+		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType, e.value, e.innerError)
 	case e.path != "" && e.valType != "" && e.value != nil:
 		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType, e.value)
-	case e.path != "" && e.valType != "" && e.innerErr != nil:
-		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType, e.innerErr)
-	case e.path != "" && e.value != nil && e.innerErr != nil:
-		return fmt.Sprintf(errMsg[e.Code], e.path, e.value, e.innerErr)
-	case e.valType != "" && e.value != nil && e.innerErr != nil:
-		return fmt.Sprintf(errMsg[e.Code], e.valType, e.value, e.innerErr)
+	case e.path != "" && e.valType != "" && e.innerError != nil:
+		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType, e.innerError)
+	case e.path != "" && e.value != nil && e.innerError != nil:
+		return fmt.Sprintf(errMsg[e.Code], e.path, e.value, e.innerError)
+	case e.valType != "" && e.value != nil && e.innerError != nil:
+		return fmt.Sprintf(errMsg[e.Code], e.valType, e.value, e.innerError)
 	case e.path != "" && e.valType != "":
 		return fmt.Sprintf(errMsg[e.Code], e.path, e.valType)
 	case e.path != "" && e.value != nil:
 		return fmt.Sprintf(errMsg[e.Code], e.path, e.value)
 	case e.valType != "" && e.value != nil:
 		return fmt.Sprintf(errMsg[e.Code], e.valType, e.value)
-	case e.innerErr != nil:
-		return fmt.Sprintf(errMsg[e.Code], e.innerErr)
+	case e.innerError != nil:
+		return fmt.Sprintf(errMsg[e.Code], e.innerError)
 	case e.value != nil:
 		return fmt.Sprintf(errMsg[e.Code], e.value)
 	case e.valType != "":
@@ -54,10 +58,14 @@ func (e NewErr) Error() string {
 const (
 	ErrUpgradedError = 1 << iota
 	ErrBadType
+	ErrBadAddressStructure
+	ErrBadAddressIndex
 )
 
 // specify the error message for each error
 var errMsg = map[int]string{
-	ErrUpgradedError: "nothing to see here",
-	ErrBadType:       "value at address [%s] is of the wrong type [%s]",
+	ErrUpgradedError:       "nothing to see here",
+	ErrBadType:             "value at address [%s] is of the wrong type [%s]",
+	ErrBadAddressStructure: "got an address mapping that does not match the formatting",
+	ErrBadAddressIndex:     "got address mapping that does not exist",
 }
--- type.go
+++ type.go
@@ -1,10 +1,97 @@
 package bossy
 
-type config interface {
-	LoadJSON() Error
-	Export()
+import "strconv"
+
+type config struct {
+	// LoadJSON() Error
+	// Export()
 }
 
-func (c config) Export() {
+func (c *config) Export() {
 
+}
+
+func atob(in string) bool {
+	checkedValues := []string{
+		"true",
+		"checked",
+		"on",
+		"yes",
+		"y",
+		"1",
+	}
+
+	for _, each := range checkedValues {
+		if each == in {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *config) setItem(address []string, loc, val interface{}) Error {
+	if len(address) < 1 {
+		return NewError{Code: BadAddressLocation}
+	}
+	switch loc := loc.(type) {
+	case map[string]interface{}:
+		// no conversion needed
+		if len(address) > 1 {
+			childLoc, ok := loc[address[0]]
+			if !ok {
+				return NewError{Code: BadAddressLocation}
+			}
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[address[0]] = val
+	case []interface{}:
+		i, err := strconv.Atoi(address[0])
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		if i < 0 || i > len(loc)-1 {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], loc[i], val)
+		}
+		loc[i] = val
+	case map[int]interface{}:
+		i, err := strconv.Atoi(address[0])
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		childLoc, ok := loc[i]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	case map[bool]interface{}:
+		v := atob(address[0])
+		childLoc, ok := loc[v]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	case map[float64]interface{}:
+		i, err := strconv.ParseFloat(address[0], 64)
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		childLoc, ok := loc[i]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	}
+	return nil
 }
