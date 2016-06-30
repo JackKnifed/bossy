4e3d4b78f2af0d3ed87fe12e08b7e27d91935e11
Jack Hayhurst <jhayhurst@liquidweb.com>
2016-06-30 00:31:29 -0400

    copied the changeItem function i had and started modifying it, pushed to save work

--- type.go
+++ type.go
@@ -29,7 +29,7 @@ func atob(in string) bool {
 	return false
 }
 
-func (c *config) setItem(address []string, loc, val interface{}) Error {
+func (c *config) changeItem(address []string, loc, val interface{}) Error {
 	if len(address) < 1 {
 		return NewError{Code: BadAddressLocation}
 	}
@@ -49,6 +49,70 @@ func (c *config) setItem(address []string, loc, val interface{}) Error {
 		if err != nil {
 			return NewError{Code: ErrBadAddressStructure}
 		}
+		if i < 0 || i > len(loc)-1 {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], loc[i], val)
+		}
+		loc[i] = val
+	case map[int]interface{}:
+		i, err := strconv.Atoi(address[0])
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		childLoc, ok := loc[i]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	case map[bool]interface{}:
+		v := atob(address[0])
+		childLoc, ok := loc[v]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	case map[float64]interface{}:
+		i, err := strconv.ParseFloat(address[0], 64)
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		childLoc, ok := loc[i]
+		if !ok {
+			return NewError{Code: ErrBadAddressIndex}
+		}
+		if len(address) > 1 {
+			return c.setItem(address[1:], childLoc, val)
+		}
+		loc[i] = val
+	}
+	return nil
+}
+
+func (c *config) setItem(address []string, loc, val interface{}) Error {
+	if len(address) < 1 {
+		return NewError{Code: BadAddressLocation}
+	}
+	switch loc := loc.(type) {
+	case map[string]interface{}:
+		// no conversion needed
+		if len(address) > 1 {
+			return c.setItem(address[1:], loc[address[0]], val)
+		}
+		loc[address[0]] = val
+	case []interface{}:
+		i, err := strconv.Atoi(address[0])
+		if err != nil {
+			return NewError{Code: ErrBadAddressStructure}
+		}
+		// TODO determine setItem behavior for slices
 		if i < 0 || i > len(loc)-1 {
 			return NewError{Code: ErrBadAddressIndex}
 		}
