d1f36eafe10e3bd538ea49df8100f8e34caa0077
James Hayhurst <James.A.Hayhurst@gmail.com>
2016-07-21 03:21:42 -0400

    i'm tired, but i started writing documentation for type.go

--- type.go
+++ type.go
@@ -5,15 +5,19 @@ type propagation struct {
 	childNode  []string
 }
 
-type restriction struct {
-	address     []string
-	restriction func(interface{}) bool
-}
+// A restriction is a function that determines whether the current level is
+// valid. You may only have one restriction per level.
+type restriction func() bool
 
+// a config is a merging of three linked trees.
+// the structure tree defines the valid/allowed structure within the config.
+//   anything that does not fit into the structure is rejected when read.
+// the defaults is another structure containing all default values - where needed.
+// finally, data contains the actual data read in.
 type config struct {
 	structure   interface{}
 	defaults    interface{}
 	data        interface{}
-	limits      []restriction
+	limit       restriction
 	propagation []propagation
 }
